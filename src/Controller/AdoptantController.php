a2231317b55bcdb0aa9cc27fcc4c4dca7257a3e8
Adel Hamza <adelhamza@hotmail.fr>
2021-10-28 15:34:01 +0200

    creation du form modification front/back

--- src/Controller/AdoptantController.php
+++ src/Controller/AdoptantController.php
@@ -3,15 +3,25 @@
 namespace App\Controller;
 
 use App\Entity\Adoptant;
+use App\Form\AdoptantCompleteFormType;
 use App\Form\AdoptantFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
 
 class AdoptantController extends AbstractController
 {
+
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
     /**
      * @Route("/form-adoptant", name="formAdoptant")
      */
@@ -33,6 +43,33 @@ class AdoptantController extends AbstractController
         }
 
         return $this->render('adoptant/formAdoptant.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/form-adoptant-update", name="updateAdoptant")
+     */
+    public function completeFormAdoptant(Request $request, EntityManagerInterface $em): Response 
+    {
+        $adoptant = $this->security->getUser();
+
+        $form = $this->createForm(AdoptantCompleteFormType::class, $adoptant, [
+            'method' => 'post'
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($adoptant);
+            $em->flush();
+
+            $this->addFlash('success', 'Compte mis à jour ! 👍');
+
+            return $this->redirectToRoute('default_index');
+        }
+
+        return $this->render('adoptant/updateAdoptant.html.twig', [
             'form' => $form->createView()
         ]);
     }
---
+++ src/Form/AdoptantCompleteFormType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Adoptant;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AdoptantCompleteFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, ['required' => true])
+            ->add('password', PasswordType::class, ['required' => true])
+            ->add('nom', TextType::class, ['required' => true])
+            ->add('adresse', TextType::class, ['required' => true])
+            ->add('ville', TextType::class, ['required' => true])
+            ->add('telephone', TextType::class, ['required' => true])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Adoptant::class,
+        ]);
+    }
+}
